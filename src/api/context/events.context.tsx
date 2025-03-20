5b0f9c7c01efec861010772834ab0fb8cb6ff24b
LeaLiv <leahl11730@gmail.com>
2025-03-20 19:12:03 +0200

    start useHttp hook

--- src/api/context/events.context.tsx
+++ src/api/context/events.context.tsx
@@ -1,5 +1,5 @@
 import { createContext } from "react";
-import { AgeGroup, EventsContextType, MyEvent } from "../../types/Event";
+import { EventsContextType, MyEvent } from "../../types/Event";
 
 export const EventsContext = createContext<Partial<EventsContextType>>({});
 
@@ -7,39 +7,36 @@ export const EventsProvider = (props: any) => {
 
     const arr:MyEvent  []=[
         {
-          id: "1",
-          name: "Men's Fitness Expo",
-          producerName: "FitLife",
-          description: "A fitness event focused on strength training and endurance for men.",
-          ageGroup: AgeGroup.men,
+            id: "1",
+            name: "פסטיבל מוזיקה חיה",
+            producerEmail: "Live Events Ltd",
+            description: "פסטיבל עם הופעות של אמנים מובילים מכל העולם",
+
         },
         {
-          id: "2",
-          name: "Women's Leadership Summit",
-          producerName: "Women Empowerment Org",
-          description: "A conference aimed at empowering women in leadership roles.",
-          ageGroup: AgeGroup.women,
+            id: "2",
+            name: "סדנת יצירה לילדים",
+            producerEmail: "KidsArt Studio",
+            description: "סדנה כיפית ליצירת יצירות אמנות בצבעי מים",
+
         },
         {
-          id: "3",
-          name: "Young Entrepreneurs Workshop",
-          producerName: "Startup Hub",
-          description: "A workshop designed to help young professionals launch their businesses.",
-          ageGroup: AgeGroup.young,
+            id: "3",
+            name: "כנס סטארט-אפים",
+            producerEmail: "TechHub",
+            description: "מפגש ליזמים, משקיעים וחובבי טכנולוגיה",
         },
         {
-          id: "4",
-          name: "Children's Science Fair",
-          producerName: "EduTech",
-          description: "A fun and educational science fair for children.",
-          ageGroup: AgeGroup.children,
+            id: "4",
+            name: "תחרות משחקי וידאו",
+            producerEmail: "eSports League",
+            description: "למדו להכין פסטות וריזוטו כמו שפים מקצועיים",
         },
         {
-          id: "5",
-          name: "Youth Sports Championship",
-          producerName: "National Sports League",
-          description: "A national championship event for young athletes.",
-          ageGroup: AgeGroup.young,
+            id: "5",
+            name: "סדנת בישול איטלקי",
+            producerEmail: "Gourmet Academy",
+            description: "למדו להכין פסטות וריזוטו כמו שפים מקצועיים",
         },
       ];
     const contextValue:EventsContextType={
